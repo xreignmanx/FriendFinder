c031bafc5233760cb7070623ce7d16263326398b
xreignmanx <rtballentine@yahoo.com>
2018-12-17 20:40:16 -0500

    friends list

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,11 @@
-//Index
+//Index page
+
+
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-//Tables page
+//survey page
 app.get('/survey', function(req,res){
     res.sendFile(path.join(__dirname, "survey.html"));
 });
